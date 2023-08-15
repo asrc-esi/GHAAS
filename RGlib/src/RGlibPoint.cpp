0448b0cd8fa1f8b0fd39fdf6b29c29d4a81b1700
Balazs Fekete <bfekete@ccny.cuny.edu>
2023-08-14 22:38:04 -0400

    Refining coordinate snapping.
    Signed-off-by: Balazs Fekete <bfekete@ccny.cuny.edu>

--- RGlib/src/RGlibPoint.cpp
+++ RGlib/src/RGlibPoint.cpp
@@ -42,7 +42,8 @@ DBInt RGlibPointSTNCoordinates(DBObjData *dbData, DBObjTableField *pField, DBObj
                 valCount++;
             }
         }
-        if ((max <= min) || (min <= 0.0)) valCount = 0;
+        if (min <= CMmathEpsilon) min = CMmathEpsilon;
+        if (max <= min) valCount = 0;
         if (pntCount > 0) { cellLength = cellLength / pntCount; maxRadius = (DBInt) ceil (radius / cellLength); }
         if (valCount > 0) { max = log(max); min = log(min); }
     }
