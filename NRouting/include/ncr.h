c831ce4a9aa36f1509595a45681a0a96894e70e5
Balazs Fekete <bfekete@ccny.cuny.edu>
2023-11-22 15:22:14 -0500

    Adding NRouting and consolidating WBM30.
    Signed-off-by: Balazs Fekete <bfekete@ccny.cuny.edu>

---
+++ NRouting/include/ncr.h
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+char *get_line(char *, int *, FILE *);
+
+typedef struct NCRnetworkCell_s {
+    size_t Id;
+    int ToCellId;
+    int BasinId;
+    float Lon;
+    float Lat;
+    float CellArea;
+    float CellLength;
+
+    float Slope;
+    float MeanDischarge;
+    float Runoff;
+    float Inflow[2];
+    float Outflow;
+    float Storage;
+    float c[3];
+} NCRnetworkCell_t;
+
+typedef struct NCRnetwork_s {
+    size_t CellNum;
+    NCRnetworkCell_t **Cells;
+} NCRnetwork_t;
+
+NCRnetwork_t *NCRnetworkLoad(const char *);
+
+void NCRnetworkFree(NCRnetwork_t *);
+
+int NCIgetDimension(int, const char *);
+
+float *NCgetFloatVector(int, const char *, size_t *);
+
+float NCRsphericalDistance(float, float, float, float);
+
+void *NCRinputOpen(NCRnetwork_t *, const char *, const char *);
+
+void NCRinputClose(void *);
+
+bool NCRinputLoad(void *, size_t, NCRnetwork_t *);
+
+float *NCRinputTimeArray(void *);
+
+bool NCRinputCopyAttributes(void *, const char *, int, int);
+
+size_t NCRinputTimeStepNum(void *);
+
+void *NCRoutputOpen(NCRnetwork_t *, const char *, const char *);
+
+void NCRoutputClose(void *);
+
+bool NCRoutputWrite(void *, size_t, NCRnetwork_t *);
+
+bool NCRoutputCopyInput(void *, void *);
+
+void NCRroutingInitialize(NCRnetwork_t *, float);
+
+void NCRroutingFunc(void *, void *, size_t);
