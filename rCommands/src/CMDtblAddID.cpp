9db8f057c4ff6ec0e5176301a9582652e6c12510
fabio1299 <fabio1299@hotmail.com>
2025-01-21 18:30:06 -0500

    Added Balazs code for tblAddID command

---
+++ rCommands/src/CMDtblAddID.cpp
@@ -0,0 +1,97 @@
+/******************************************************************************
+
+GHAAS RiverGIS Utilities V3.0
+Global Hydrological Archive and Analysis System
+Copyright 1994-2025, UNH - CCNY
+
+CMDtblAddIdID.cpp
+
+bfekete@ccny.cuny.edu
+
+*******************************************************************************/
+
+#include <cm.h>
+#include <DB.hpp>
+#include <DBif.hpp>
+#include <RG.hpp>
+
+static void _CMDprintUsage (const char *arg0) {
+    CMmsgPrint(CMmsgInfo, "%s [options] <input file> <output file>", CMfileName(arg0));
+    CMmsgPrint(CMmsgInfo, "     -a, --table     [table name]");
+    CMmsgPrint(CMmsgInfo, "     -f, --field     [field name]");
+    CMmsgPrint(CMmsgInfo, "     -h, --help");
+}
+
+int main(int argc, char *argv[]) {
+    int argPos, argNum = argc, ret;
+    DBInt recID;
+    DBObjData *data;
+    char *tableName = (char *) NULL;
+    char *idFieldName = (char *) NULL;
+    DBObjTable *table;
+    DBObjTableField *idField;
+    DBObjRecord *record;
+ 
+    for (argPos = 1; argPos < argNum;) {
+        if (CMargTest (argv[argPos], "-a", "--table")) {
+            if ((argNum = CMargShiftLeft(argPos, argv, argNum)) <= argPos) {
+                CMmsgPrint(CMmsgUsrError, "Missing table name!");
+                return (CMfailed);
+            }
+            tableName = argv[argPos];
+            if ((argNum = CMargShiftLeft(argPos, argv, argNum)) <= argPos) break;
+            continue;
+        }
+        if (CMargTest (argv[argPos], "-f", "--field")) {
+            if ((argNum = CMargShiftLeft(argPos, argv, argNum)) <= argPos) {
+                CMmsgPrint(CMmsgUsrError, "Missing field name!");
+                return (CMfailed);
+            }
+            idFieldName = argv[argPos];
+            if ((argNum = CMargShiftLeft(argPos, argv, argNum)) <= argPos) break;
+            continue;
+        }
+        if (CMargTest (argv[argPos], "-h", "--help")) {
+            _CMDprintUsage(argv[0]);
+            return (DBSuccess);
+        }
+        if ((argv[argPos][0] == '-') && (strlen(argv[argPos]) > 1)) {
+            CMmsgPrint(CMmsgUsrError, "Unknown option: %s!", argv[argPos]);
+            return (CMfailed);
+        }
+        argPos++;
+    }
+
+    if (argNum > 3) {
+        CMmsgPrint(CMmsgUsrError, "Extra arguments!");
+        _CMDprintUsage (argv[0]);
+        return (CMfailed);
+    }
+    data = new DBObjData();
+    if (((argNum > 1) && (strcmp(argv[1], "-") != 0) ? data->Read(argv[1]) : data->Read(stdin)) == DBFault) {
+        delete data;
+        return (CMfailed);
+    }
+
+    if (tableName == (char *) NULL) tableName = (char *) "DBItems";
+    if ((table = data->Table(tableName)) == (DBObjTable *) NULL) {
+        CMmsgPrint(CMmsgUsrError, "Invalid table: %s!", tableName);
+        delete data;
+        return (CMfailed);
+    }
+
+    idFieldName = idFieldName == (char *) NULL ? (char *) "IDfield" : idFieldName;
+    idField = table->Field (idFieldName);
+    if (idField == (DBObjTableField *) NULL) {
+        idField  = new DBObjTableField (idFieldName, DBTableFieldInt, (char *) "%10d", sizeof (DBInt));
+        table->AddField(idField);
+    }
+    for (recID = 0; recID < table->ItemNum(); ++recID) {
+        record = table->Item(recID);
+        idField->Int(record,recID + 1);
+    }
+
+    ret = (argNum > 2) && (strcmp(argv[2], "-") != 0) ? data->Write(argv[2]) : data->Write(stdout);
+    delete data;
+    return (ret);
+}
